5c59b51a139a1a7967545358cd17e977202fe301
Shailndra <shailndrasingh01@gmail.com>
2024-09-19 23:52:20 +0530

    newcode-commit

---
+++ by_days/day4.py
@@ -0,0 +1,9 @@
+# name = "my name is shailendra"
+# # something1 = name.split()
+# print("something1:", something1[2])
+
+
+name = "my name is shailendra"
+# something1 = name.split()
+upper = name.upper()
+print("something1:", upper)
